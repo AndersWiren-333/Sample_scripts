d3b04423789f3aaccc0a24820db9e8bea351ef76
Anders Wiren <anders.mikael.wiren@gmail.com>
2017-10-20 12:45:07 +0200

    Applied fix to universal headers to all scripts/module
    The fix fixes the function in the module and script headers that finds the absolute paths
    to this specific repository's script- and module folders.

--- scripts/length_filter_for_fasta_files_1.0.pl
+++ scripts/length_filter_for_fasta_files_1.0.pl
@@ -18,10 +18,11 @@ use diagnostics;
 # Set paths to scripts and modules. Setting explicit paths to the scripts and modules in this specific repository (rather than adding paths to @INC, PERLLIB and PATH on
 # your local system) avoids the risk of scripts calling the wrong scripts/modules if you have other repositories on your system that happen to have some script- and module names
 # in common with this repository.
-my $scripts = cwd;
-chdir("../modules");
-my $modules = cwd;
-chdir("../scripts");
+my $thisfile = (__FILE__);
+my $scripts = "";
+if($thisfile =~ m/^(.+)\//)	{	$scripts = $1;	}
+my $modules = $scripts;
+$modules =~ s/scripts/modules/;
 
 # If this script/module is intended to be used outside the folder structure of the parent repository (e.g. a wrapper script to be started from
 # another part of your system), set the absolute path to repository scripts and modules (that this cript may depend on) here (and comment out
